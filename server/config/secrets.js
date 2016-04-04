bf4c512866c715c8a028d197de7b1b6b21096de6
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-04-05 00:24:52 +0200

    linked external db

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,10 +1,6 @@
-/** Important **/
-/** You should not be committing this file to GitHub **/
-/** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
-
 module.exports = {
   // Find the appropriate database to connect to, default to localhost if not found.
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/ReactWebpackNode',
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://tymoteuszb:ScrapSwapMongo2016@ds015710.mlab.com:15710/io-scrapswap',
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
     clientID: process.env.GOOGLE_CLIENTID || '62351010161-eqcnoa340ki5ekb9gvids4ksgqt9hf48.apps.googleusercontent.com',
