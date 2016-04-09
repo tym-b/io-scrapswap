8cd530188d3c391a5c287d25c4a9a0a72b0eadd9
pawelhusak <pawelhusak@gmail.com>
2016-04-09 19:55:12 +0200

    Zmiana ClientID i ClientSecret dla logowania Google

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -3,8 +3,8 @@ module.exports = {
   db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://scrapswap:scrapswap@ds015710.mlab.com:15710/io-scrapswap',
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
-    clientID: process.env.GOOGLE_CLIENTID || '62351010161-eqcnoa340ki5ekb9gvids4ksgqt9hf48.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU',
+    clientID: process.env.GOOGLE_CLIENTID || '808748927391-ve2qs2nn6uam7m9md4foj3oh2dv8c05p.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_SECRET || 'orLJPQ765LEb_58yZwD6TznB',
     callbackURL: process.env.GOOGLE_CALLBACK || "/auth/google/callback"
   }
 };
