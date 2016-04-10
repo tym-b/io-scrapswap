057befd116b659428578631ccec7254cdf7ebf8e
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-04-10 12:11:16 +0200

    removed mongo url

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Find the appropriate database to connect to, default to localhost if not found.
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://scrapswap:scrapswap@ds015710.mlab.com:15710/io-scrapswap',
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI,
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
     clientID: process.env.GOOGLE_CLIENTID || '808748927391-ve2qs2nn6uam7m9md4foj3oh2dv8c05p.apps.googleusercontent.com',
