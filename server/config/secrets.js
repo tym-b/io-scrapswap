4ee85f9ca67fa0775736cb34b9fb8d27c5d07cd8
pawelhusak <pawelhusak@gmail.com>
2016-04-12 16:56:04 +0200

    removed google secrets and added local mongoDB url

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Find the appropriate database to connect to, default to localhost if not found.
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI,
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/scrapswapdb',
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
-    clientID: process.env.GOOGLE_CLIENTID || '808748927391-ve2qs2nn6uam7m9md4foj3oh2dv8c05p.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_SECRET || 'orLJPQ765LEb_58yZwD6TznB',
-    callbackURL: process.env.GOOGLE_CALLBACK || "/auth/google/callback"
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
   }
 };
