2d0b5b1c048c20b5e8ff6c1b24fc9c6e209ee9f6
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-04-19 22:24:56 +0200

    adverts container added

---
+++ app/containers/AdvertList.jsx
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+class AdvertListContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AdvertList adverts={this.props.adverts} />
+    );
+  }
+}
+
+AdvertListContainer.propTypes = {
+  adverts: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    adverts: state.advert.adverts
+  };
+}
+
+export default connect(mapStateToProps)(AdvertListContainer);
