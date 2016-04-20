8656622d4f57c33d1869353eb0ca41d5b3bd0473
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-04-21 00:43:03 +0200

    extracted app header

---
+++ app/components/AppHeader.jsx
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import AccountIcon from 'material-ui/lib/svg-icons/action/account-circle';
+
+export default class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AppBar title={<Link to="/">ScrapSwap</Link>} showMenuIconButton={false} iconElementRight={<Link to="/login"><IconButton><AccountIcon /></IconButton></Link>} />
+    );
+  }
+}
+
+AppHeader.propTypes = {
+  logged: PropTypes.bool.isRequired
+};
