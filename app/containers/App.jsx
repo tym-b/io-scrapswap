08e0695df4d16bca27e762613db64bb854adc2e3
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-05-04 21:07:54 +0200

    fixed server side rendering

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -4,7 +4,32 @@ import Message from 'containers/Message';
 import classNames from 'classnames/bind';
 import styles from 'css/main';
 
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import themeDecorator from 'material-ui/lib/styles/theme-decorator';
+import colors from 'material-ui/lib/styles/colors';
+
 const cx = classNames.bind(styles);
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: colors.green500,
+    primary2Color: colors.green700,
+    primary3Color: colors.green100,
+  },
+}, {
+  avatar: {
+    borderColor: null,
+  },
+  userAgent: 'all'
+});
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
 
 const App = ({children}) => {
   return (
@@ -20,4 +45,4 @@ App.propTypes = {
   children: PropTypes.object
 };
 
-export default App;
+export default themeDecorator(muiTheme)(App);
