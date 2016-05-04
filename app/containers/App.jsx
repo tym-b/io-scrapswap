3aec3433b435eee182dcb6834b950a87c894bab0
tymoteuszb <tymoteusz.bleja@outlook.com>
2016-05-04 22:29:05 +0200

    fixed css hmr

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,19 +1,24 @@
 import React, { PropTypes } from 'react';
 import Navigation from 'containers/Navigation';
+import classNames from 'classnames/bind';
+import styles from 'css/main';
 
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 import themeDecorator from 'material-ui/lib/styles/theme-decorator';
 import colors from 'material-ui/lib/styles/colors';
 
+const cx = classNames.bind(styles);
+
 const muiTheme = getMuiTheme({
-  fontFamily: 'Roboto, sans-serif',
   palette: {
     primary1Color: colors.green500,
     primary2Color: colors.green700,
     primary3Color: colors.green100,
-    textColor: colors.fullWhite
   },
 }, {
+  avatar: {
+    borderColor: null,
+  },
   userAgent: 'all'
 });
 
