13f191a83fa8d96fcc8deb75a4bc310b23ff3c3e
Pawel Husak <pawelhusak@gmail.com>
2016-05-26 23:55:56 +0200

    Added messaging system

--- server/models/conversation.js
+++ server/models/conversation.js
@@ -4,7 +4,8 @@ var ConversationSchema = new mongoose.Schema({
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
-  hasUnreadMessages: { type: Boolean, default: false }
+  hasUnreadMessages: { type: Boolean, default: false },
+  lastModificationDate: { type: Date, default: Date.now }
 });
 
 Conversation = mongoose.model('Conversation', ConversationSchema);
